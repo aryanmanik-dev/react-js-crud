2ab4cac46e5b1f76df6eca9249899340a1af1d36
aryanmanik013 <aryanmanik440@gmail.com>
2024-01-12 17:59:17 +0530

    frontend changes

--- fe/src/Components/Header.tsx
+++ fe/src/Components/Header.tsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
+  const navigateTo = useNavigate();
+
+  const handleLogout = () => {
+    navigateTo('/');
+  };
+
   return (
     <>
-        <nav className="navbar bg-light"></nav>
+      <nav className="navbar bg-light">
+        <button onClick={handleLogout} className="btn btn-primary ms-auto logout-btn">
+          Logout
+        </button>
+      </nav>
     </>
   );
 };
